50b0e616e180e5564e80179cee4420fcbf854724
Silas Wolf <azolyth@gmail.com>
2022-10-15 11:48:30 -0700

    Added more card data

--- src/data.js
+++ src/data.js
@@ -1,13 +1,37 @@
 export default [
   {
     id: 1,
-    title: 'Mount Fuji',
-    location: 'Japan',
+    title: 'Paris',
+    location: 'France',
     googleMapsUrl: 'https://goo.gl/maps/1DGM5WrWnATgkSNB8',
-    startDate: '12 Jan, 2021',
-    endDate: '24 Jan, 2021',
+    startDate: '4 Jul, 2021',
+    endDate: '17 Jul, 2021',
     description:
-      'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
-    imageUrl: 'https://source.unsplash.com/WLxQvbMyfas',
+      'The City of Paris is the centre of the region and province of Île-de-France, or Paris Region, with an estimated population of 12,262,544 in 2019, or about 19% of the population of France.',
+    imageUrl:
+      'https://images.unsplash.com/photo-1522093007474-d86e9bf7ba6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80',
+  },
+  {
+    id: 2,
+    title: 'Geirangerfjord',
+    location: 'Norway',
+    googleMapsUrl: 'https://goo.gl/maps/1DGM5WrWnATgkSNB8',
+    startDate: '27 May, 2021',
+    endDate: '8 Jun, 2021',
+    description:
+      'The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.',
+    imageUrl: 'https://source.unsplash.com/3PeSjpLVtLg',
+  },
+  {
+    id: 3,
+    title: 'Copenhagen',
+    location: 'Denmark',
+    googleMapsUrl: 'https://goo.gl/maps/1DGM5WrWnATgkSNB8',
+    startDate: '12 Nov, 2021',
+    endDate: '22 Nov, 2021',
+    description:
+      "Copenhagen's surrounding region is easy to reach and is packed with beatiful landscapes and cozy beach towns, historic castles and modern museums that all make for a perfect day trip.",
+    imageUrl:
+      'https://images.unsplash.com/photo-1552560880-2482cef14240?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=886&q=80',
   },
 ];
