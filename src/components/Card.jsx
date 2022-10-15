dfad3602f864c40070328f3b61178e032ac17940
Silas Wolf <azolyth@gmail.com>
2022-10-14 23:07:14 -0700

    Styled Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 
+// Images
+import pin from '../images/pin.png';
+
 const Card = (props) => {
   return (
     <section className='card'>
       <img className='card-img' src={props.imageUrl} />
       <div className='card-info'>
-        <p className='location'>{props.location}</p>
-        <a className='maps-link' href={props.googleMapsUrl}>
-          View on Google Maps
-        </a>
+        <div className='card-location'>
+          <img className='pin-img' src={pin} />
+          <p className='location light'>{props.location}</p>
+          <a className='maps-link light' href={props.googleMapsUrl}>
+            View on Google Maps
+          </a>
+        </div>
         <h2 className='card-title bold'>{props.title}</h2>
-        <p className='bold'>
+        <p className='date bold'>
           {props.startDate} - {props.endDate}
         </p>
-        <p className='card-desc'>{props.description}</p>
+        <p className='card-desc light'>{props.description}</p>
       </div>
     </section>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+// Images
+
+import globe from '../images/globe.png';
+
 const Nav = () => {
   return (
     <nav>
+      <img className='globe-img' src={globe} />
       <h3 className='nav-title'>my travel journal.</h3>
     </nav>
   );
